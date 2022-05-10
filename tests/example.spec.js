6a121401fb2257fecbfd4573e62eb6a733bafa8b
admin <michalqa1234>
2022-05-10 10:28:33 +0200

    test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,11 +1,9 @@
-const { test } = require("@playwright/test");
+let { test } = require("@playwright/test");
 
 test("check on Teonite website is Defguard exist", async function ({ page }) {
   await page.goto("https://teonite.com");
-  await page
-      .locator("#navbarCollapse > ul > li:nth-child(3) > a")
+  await page.locator("#navbarCollapse > ul > li:nth-child(3) > a")
       .click();
-  await page
-      .locator("#ventures-portfolio > div:nth-child(2) > div > div:nth-child(4) > div > div > h3 > img")
-      .click();
+  await page.locator('text=DefGuard TEONITE Ventures')
+      .isVisible();
 });
