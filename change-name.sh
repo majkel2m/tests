8879d9396011f27f99e543f797a60fc93c8b0fe9
admin <michalqa1234>
2022-05-19 09:38:53 +0200

    first step

---
+++ change-name.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CURRENT_DATE= `date`
+echo ${CURRENT_DATE}
+NEW_NAME='./dummy_file.'${CURRENT_DATE}'.txt'
+echo ${NEW_NAME}
+mv ./dummy_file.txt ${NEW_NAME}
+
+
+ZMIENNA=""
+if [ -n "$ZMIENNA" ]; then # -n sprawdza czy argument nie jest pusty
+ echo ” Zmienna o nazwie ZMIENNA nie jest pusta”;
+else
+ echo ” Zmienna o nazwie ZMIENNA jest pusta”;
+fi
