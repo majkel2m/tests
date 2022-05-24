f79d09dab125e9f9a7095b9eeaa71a7fd47dd09e
admin <michalqa1234>
2022-05-24 21:09:21 +0200

    done

--- change-name.sh
+++ change-name.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-CURRENT_DATE= `date`
-echo ${CURRENT_DATE}
-NEW_NAME='./dummy_file.'${CURRENT_DATE}'.txt'
-echo ${NEW_NAME}
-mv ./dummy_file.txt ${NEW_NAME}
+#CURRENT_DATE= `date`
+#echo ${CURRENT_DATE}
+#NEW_NAME='./dummy_file.'${CURRENT_DATE}'.txt'
+#echo ${NEW_NAME}
+#mv ./dummy_file.txt ${NEW_NAME}
+#
+#
+#ZMIENNA=""
+#if [ -n "$ZMIENNA" ]; then # -n sprawdza czy argument nie jest pusty
+# echo ” Zmienna o nazwie ZMIENNA nie jest pusta”;
+#else
+# echo ” Zmienna o nazwie ZMIENNA jest pusta”;
+#fi
 
-
-ZMIENNA=""
-if [ -n "$ZMIENNA" ]; then # -n sprawdza czy argument nie jest pusty
- echo ” Zmienna o nazwie ZMIENNA nie jest pusta”;
-else
- echo ” Zmienna o nazwie ZMIENNA jest pusta”;
-fi
+#!/bin/bash
+echo ”Podaj swoje imie:”
+read IMIE
+echo ”Na imie masz: $IMIE”
